64d8994d66004e479e148af64682a537dcde629e
Joker Zhong <jiahaozhongjoker@gmail.com>
2023-07-30 05:37:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Place this code at the beginning of your script or inside an initialization function
+// Parameters
+let selectedBrands = ['Toyota', 'Ford'];
+
+// Event Listener for Checkboxes
 d3.selectAll("input[type=checkbox]").on("change", function() {
   if (this.checked) {
     selectedBrands.push(this.value);
@@ -8,129 +11,148 @@ d3.selectAll("input[type=checkbox]").on("change", function() {
   createScene3(); // Update the scene
 });
 
-
-let selectedBrands = ['Toyota', 'Ford'];
-
-d3.csv("cars2017.csv").then(data => {
-  const groupedData = d3.group(data, d => d.Brand);
-  const averageMileage = Array.from(groupedData).map(([key, value]) => {
-    const avg = d3.mean(value, d => +d.Mileage);
-    return { Brand: key, Mileage: avg };
-  });
-  createScene1(averageMileage);
-});
-
-function createScene1(data) {
-  // Select and clear the SVG container
-  const svg = d3.select("#visualization").html("");
-
-  // Define scales
-  const xScale = d3.scaleBand().domain(data.map(d => d.Brand)).range([0, 800]);
-  const yScale = d3.scaleLinear().domain([0, d3.max(data, d => d.Mileage)]).range([600, 0]);
-
-  // Draw bars
-  svg.selectAll("rect")
-    .data(data)
-    .enter().append("rect")
-    .attr("x", d => xScale(d.Brand))
-    .attr("y", d => yScale(d.Mileage))
-    .attr("width", xScale.bandwidth())
-    .attr("height", d => 600 - yScale(d.Mileage))
-    .attr("fill", "steelblue");
-
-  // Add axes
-  svg.append("g").attr("transform", "translate(0,600)").call(d3.axisBottom(xScale));
-  svg.append("g").call(d3.axisLeft(yScale));
-
-  const highestMileageBrand = d3.max(data, d => d.Mileage);
-  const annotations = [
-    {
-      note: { label: "Highest average mileage" },
-      x: xScale(highestMileageBrand.Brand),
-      y: yScale(highestMileageBrand.Mileage),
-      dy: -30,
-      dx: 0
-    }
-  ];
-
-  const makeAnnotations = d3.annotation().annotations(annotations);
-  svg.append("g").call(makeAnnotations);
-}
-
-function createScene2() {
-  d3.csv("cars2017.csv").then(data => {
-    const top5Cars = data.sort((a, b) => b.Mileage - a.Mileage).slice(0, 5);
-
-    // Select and clear the SVG container
+// Scene 1: Overview of All Car Brands and Their Average Mileage
+function createScene1() {
+  d3.csv("https://flunky.github.io/cars2017.csv").then(data => {
     const svg = d3.select("#visualization").html("");
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const width = +svg.attr("width") - margin.left - margin.right;
+    const height = +svg.attr("height") - margin.top - margin.bottom;
+    const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
-    // Define scales
-    const xScale = d3.scaleBand().domain(top5Cars.map(d => d.Car)).range([0, 800]);
-    const yScale = d3.scaleLinear().domain([0, d3.max(top5Cars, d => +d.Mileage)]).range([600, 0]);
+    const xScale = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+    const yScale = d3.scaleLinear().rangeRound([height, 0]);
 
-    // Draw bars
-    svg.selectAll("rect")
-      .data(top5Cars)
+    xScale.domain(data.map(d => d.Brand));
+    yScale.domain([0, d3.max(data, d => d.Mileage)]);
+
+    g.append("g")
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(xScale));
+
+    g.append("g")
+      .call(d3.axisLeft(yScale));
+
+    g.selectAll(".bar")
+      .data(data)
       .enter().append("rect")
-      .attr("x", d => xScale(d.Car))
+      .attr("class", "bar")
+      .attr("x", d => xScale(d.Brand))
       .attr("y", d => yScale(d.Mileage))
       .attr("width", xScale.bandwidth())
-      .attr("height", d => 600 - yScale(d.Mileage))
-      .attr("fill", "green");
+      .attr("height", d => height - yScale(d.Mileage));
 
-    // Add axes
-    svg.append("g").attr("transform", "translate(0,600)").call(d3.axisBottom(xScale));
-    svg.append("g").call(d3.axisLeft(yScale));
+    // Annotations for Scene 1
+    const annotations = [
+      {
+        note: { label: "Highest average mileage" },
+        x: xScale("Toyota"), // Example
+        y: yScale(35), // Example
+        dy: -30,
+        dx: 0
+      }
+    ];
+    const makeAnnotations = d3.annotation().annotations(annotations);
+    g.append("g").call(makeAnnotations);
   });
-
-  const mostEfficientCar = d3.max(top5Cars, d => d.Mileage);
-  const annotations = [
-    {
-      note: { label: "Most fuel-efficient car" },
-      x: xScale(mostEfficientCar.Car),
-      y: yScale(mostEfficientCar.Mileage),
-      dy: -30,
-      dx: 0
-    }
-  ];
-
-  const makeAnnotations = d3.annotation().annotations(annotations);
-  svg.append("g").call(makeAnnotations);
 }
 
-function createScene3() {
-  d3.csv("cars2017.csv").then(data => {
-    // Filter data for selected brands (e.g., 'Toyota', 'Ford')
-    const selectedData = data.filter(d => selectedBrands.includes(d.Brand));
-
-    // Select and clear the SVG container
+// Scene 2: Focus on the Top 5 Most Fuel-Efficient Cars (Line Chart)
+function createScene2() {
+  d3.csv("https://flunky.github.io/cars2017.csv").then(data => {
+    const top5Cars = data.sort((a, b) => b.Mileage - a.Mileage).slice(0, 5);
     const svg = d3.select("#visualization").html("");
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const width = +svg.attr("width") - margin.left - margin.right;
+    const height = +svg.attr("height") - margin.top - margin.bottom;
+    const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
-    // Define scales
-    const xScale = d3.scaleLinear().domain([0, d3.max(selectedData, d => +d.Horsepower)]).range([0, 800]);
-    const yScale = d3.scaleLinear().domain([0, d3.max(selectedData, d => +d.Price)]).range([600, 0]);
+    const xScale = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+    const yScale = d3.scaleLinear().rangeRound([height, 0]);
 
-    // Draw circles
-    svg.selectAll("circle")
+    xScale.domain(top5Cars.map(d => d.Car));
+    yScale.domain([0, d3.max(top5Cars, d => d.Mileage)]);
+
+    g.append("g")
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(xScale));
+
+    g.append("g")
+      .call(d3.axisLeft(yScale));
+
+    // Line generator
+    const line = d3.line()
+      .x(d => xScale(d.Car))
+      .y(d => yScale(d.Mileage));
+
+    // Draw line
+    g.append("path")
+      .datum(top5Cars)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+
+    // Annotations for Scene 2
+    const annotations = [
+      {
+        note: { label: "Most fuel-efficient car" },
+        x: xScale("Example Car"), // Example
+        y: yScale(35), // Example
+        dy: -30,
+        dx: 0
+      }
+    ];
+    const makeAnnotations = d3.annotation().annotations(annotations);
+    g.append("g").call(makeAnnotations);
+  });
+}
+
+// Scene 3: Comparison Between Horsepower and Price for Selected Brands (Scatter Plot)
+function createScene3() {
+  d3.csv("https://flunky.github.io/cars2017.csv").then(data => {
+    const selectedData = data.filter(d => selectedBrands.includes(d.Brand));
+    const svg = d3.select("#visualization").html("");
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const width = +svg.attr("width") - margin.left - margin.right;
+    const height = +svg.attr("height") - margin.top - margin.bottom;
+    const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const xScale = d3.scaleLinear().range([0, width]);
+    const yScale = d3.scaleLinear().range([height, 0]);
+
+    xScale.domain([0, d3.max(selectedData, d => d.Horsepower)]);
+    yScale.domain([0, d3.max(selectedData, d => d.Price)]);
+
+    g.append("g")
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(xScale));
+
+    g.append("g")
+      .call(d3.axisLeft(yScale));
+
+    g.selectAll(".dot")
       .data(selectedData)
       .enter().append("circle")
+      .attr("class", "dot")
       .attr("cx", d => xScale(d.Horsepower))
       .attr("cy", d => yScale(d.Price))
-      .attr("r", 5)
-      .attr("fill", d => d.Brand === 'Toyota' ? "red" : "blue");
+      .attr("r", 5);
 
-    // Add axes
-    svg.append("g").attr
-
-    const toyotaCars = selectedData.filter(d => d.Brand === 'Toyota');
-  const annotations = toyotaCars.map(car => ({
-    note: { label: car.Car },
-    x: xScale(car.Horsepower),
-    y: yScale(car.Price),
-    dy: -10,
-    dx: 0
-  }));
-
-  const makeAnnotations = d3.annotation().annotations(annotations);
-  svg.append("g").call(makeAnnotations);
+    // Annotations for Scene 3
+    const annotations = selectedData.map(car => ({
+      note: { label: car.Car },
+      x: xScale(car.Horsepower),
+      y: yScale(car.Price),
+      dy: -10,
+      dx: 0
+    }));
+    const makeAnnotations = d3.annotation().annotations(annotations);
+    g.append("g").call(makeAnnotations);
+  });
 }
+
+// Create the first scene on
+createScene1();
